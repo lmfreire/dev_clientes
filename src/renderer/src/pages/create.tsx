6cb6b925e743c445ba2e7e2c4833866c29cd8703
lmfreire <l.matheus.freire10@gmail.com>
2025-01-15 12:44:40 -0300

    form de cadastro de cliente

--- src/renderer/src/pages/create.tsx
+++ src/renderer/src/pages/create.tsx
@@ -1,28 +1,126 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { NewCustomer } from "../../../shared/types/ipc"
+import { FormEvent, useRef } from 'react'
+import { useNavigate } from "react-router-dom";
+
+interface DataMutation {
+    name: string;
+    email: string;
+    phone: string;
+    address: string
+    role: string
+}
 
 export function Create(){
+    const navigate = useNavigate()
+    const queryClient = useQueryClient();
+    const nameRef = useRef<HTMLInputElement | null>(null)
+    const emailRef = useRef<HTMLInputElement | null>(null)
+    const addressRef = useRef<HTMLInputElement | null>(null)
+    const roleRef = useRef<HTMLInputElement | null>(null)
+    const phoneRef = useRef<HTMLInputElement | null>(null)    
 
-    async function handleAddCustomer() {
+    const {isPending, mutateAsync: CreateCustomer} = useMutation({mutationFn: async (data: DataMutation) => {
 
-        const doc: NewCustomer = {
-            name: "matheus f",
-            email: "matheus@gmail.com",
-            phone: "11999999999",
-            address: "Rua A, 123",
-            role: "FrontEnd",
+        await window.api.addCustomer({
+            ...data,
             status: true
+        }).then((res) => {
+            console.log("DEU CERTO O CADASTRO");
+            nameRef.current!.value = "";
+            emailRef.current!.value = "";
+            addressRef.current!.value = "";
+            roleRef.current!.value = "";
+            phoneRef.current!.value = "";
+            // navigate("/");
+        }).catch((err) => {
+            console.log("ERRO AO CADASTRAR: ", err);            
+        });
+
+    },
+    onSuccess: () => {
+        queryClient.invalidateQueries({queryKey: 'customers'});
+    }
+    })
+
+    async function handleAddCustomer(e: FormEvent) {
+        e.preventDefault();
+
+        const name = nameRef.current?.value;
+        const email = emailRef.current?.value;
+        const address = addressRef.current?.value;
+        const role = roleRef.current?.value;
+        const phone = phoneRef.current?.value;
+
+        if (!name || !email || !address || !role || !phone) {
+            return;            
         }
 
-        const response = await window.api.addCustomer(doc)
-        console.log(response);
-        
+        await CreateCustomer({name, email, address, role, phone});
     }
 
     return(
-        <div>
-            <h1>Pagina Novo Cliente</h1>
+        <div className="flex-1 flex flex-col py-12 px-10 gap-8 overflow-y-auto">
 
-            <button onClick={handleAddCustomer}>CADASTRAR</button>
+            <section className="flex flex-1 flex-col items-center">
+                <h1 className="text-white text-xl lg:text-2xl font-semibold">Cadastrar Novo Cliente</h1>
+                <form className="w-full max-w-96 mt-4" onSubmit={handleAddCustomer}>
+                    <div className="mb-2">
+                        <label className="text-lg">Nome: </label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite nome do cliente..."
+                            className="w-full h-9 rounded text-black px-2"
+                            ref={nameRef}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="text-lg">Endereço: </label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite seu endereço completo..."
+                            className="w-full h-9 rounded text-black px-2"
+                            ref={addressRef}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="text-lg">Email: </label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite email..."
+                            className="w-full h-9 rounded text-black px-2"
+                            ref={emailRef}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="text-lg">Cargo: </label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o Cargo..."
+                            className="w-full h-9 rounded text-black px-2"
+                            ref={roleRef}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="text-lg">Telefone: </label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o telefone..."
+                            className="w-full h-9 rounded text-black px-2"
+                            ref={phoneRef}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="bg-blue-500 rounded flex items-center justify-center w-full h-9 text-white disabled:bg-gray-500"
+                        disabled={isPending}
+                    >
+                        Cadastrar
+                    </button>
+                </form>
+            </section>
+
         </div>
     )
 }
